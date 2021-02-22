348bfc19f6ab75bf8b235129c359266a01b71c85
MadMax <maxviper89@gmail.com>
2021-02-22 02:07:39 +0200

    add common

---
+++ src/components/common/button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button as MuiButton, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: "#008cf0",
+    color: "#FFFFFF",
+    borderRadius: 20,
+    fontSize: "16px",
+    fontFamily: "Ubuntu, sans-serif",
+    fontWeight: 300,
+    "&:hover": {
+      backgroundColor: "#3796f8",
+    },
+  },
+  label: {
+    textTransform: "none",
+  },
+}));
+
+export default function Button(props) {
+  const { text, size, color, variant, onClick, ...other } = props;
+  const classes = useStyles();
+
+  return (
+    <MuiButton
+      variant={variant || "contained"}
+      size={size || "large"}
+      onClick={onClick}
+      {...other}
+      classes={{ root: classes.root, label: classes.label }}
+    >
+      {text}
+    </MuiButton>
+  );
+}
